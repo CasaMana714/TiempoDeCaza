cc305de0e6ef188954f9e164ad85ad3526faecac
jairobandera <bandera.jairo@gmail.com>
2021-11-29 17:21:12 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+let radio = document.getElementById("radio");
+let boton = document.getElementById("playerButton");
+
+let boton2 = document.getElementById("playerButton2");
+
+
+function reproducirAudio() {
+    radio.play();
+    boton.style.display = "none";
+    boton2.style.display = "block";
+}
+
+function pausarAudio(){
+    radio.pause();
+    boton.style.display = "block";
+    boton2.style.display = "none";
+}
+
